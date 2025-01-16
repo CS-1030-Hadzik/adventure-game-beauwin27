694a302347420eee2c73972e93d3663af71220f1
beaugw27 <beauwinward@mail.weber.edu>
2025-01-16 10:03:21 -0700

    ADD: Docstring for program info

--- game.py
+++ game.py
@@ -1,3 +1,13 @@
+'''
+Adventure Game
+Author: Beau Winward
+Version: 1.0
+Description:
+This is a text-based adventure game where the player makes choices
+to navigate through a mysterious forest.
+'''
+
+
+# Welcom message and introduction
 print("Welcome to the Adventure Game!")
 print("Your journey begins here...")
-
